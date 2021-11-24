186f1b8aaa633c1fdd326dcb6ef727371af1f5b1
QingTianSmell <QingTianSmell@gmail.com>
2021-11-24 11:37:36 +0800

    init

---
+++ src/test/java/top/orionpax/learnspringinitializrdependencies/LearnSpringInitializrDependenciesApplicationTests.java
@@ -0,0 +1,13 @@
+package top.orionpax.learnspringinitializrdependencies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringInitializrDependenciesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
